5d5775a749d7637b6b30790662d484f1c78db258
alan-dotcom23 <bastianmv123@gmail.com>
2026-04-28 13:32:59 -0400

    Agrega: 7 microservicios agregados

---
+++ Auth-Service-Accesos/src/test/java/com/ProyectoRestaurant/Auth/Service/Accesos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoRestaurant.Auth.Service.Accesos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Order-Service-Comandas/src/test/java/com/ProyectoRestaurant/Order/Service/Comandas/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoRestaurant.Order.Service.Comandas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
