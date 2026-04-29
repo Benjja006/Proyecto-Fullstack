8b961f1ce3a8970a467240f1bc0f8971838aa4e4
alan-dotcom23 <bastianmv123@gmail.com>
2026-04-29 08:50:27 -0400

    Agregar: ultimos 10 archivos

---
+++ Inventory Service (Stock)/src/test/java/com/ProyectoRestaurant/inventory/Service/stok/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoRestaurant.inventory.Service.stok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
